f53e92561959209c65e2169cdd45a61cc230475e
Kevin Hannay <khannay@arcascope.com>
2021-11-07 14:33:06 -0600

    Initial Commit

---
+++ ParrotSpecies/ParrotSpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotSpeciesApp.swift
+//  ParrotSpecies
+//
+//  Created by Kevin Hannay on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ParrotSpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
